e5a95b49f2fba5eb88e7217f4b3516a66e0c46c5
Ranjan2104 <ranjan.amresh100@gmail.com>
2022-12-05 16:33:09 +0530

    update file

--- theme/sections/hero-imageF.vue
+++ theme/sections/hero-imageF.vue
@@ -55,7 +55,7 @@
   /deep/ .fy__img {
     width: 100%;
     @media @mobile{
-      display: flex;
+      display: none;
     }
   }
 }
@@ -95,82 +95,53 @@
 
 .overlay {
   position: absolute;
-  top: 55%;
+  top: 30%;
   left: 50%;
   width: 100%;
   transform: translate(-50%, -50%);
   // display: flex;
-  &__content {
-    flex-direction: column;
-    padding: 0 @header-padding-desktop;
-    width: 30%;
-    align-items: center;
-    text-align: center;
-    @media @mobile {
-      width: 100%;
-      padding: 0;
-    }
-  }
-  &__left {
-    align-items: flex-start;
-    justify-content: flex-start;
-    @media @mobile {
-      align-items: center;
-      justify-content: center;
-    }
-    .overlay__content {
-      // display: contents;
-      align-items: flex-start;
-      text-align: left;
-      @media @mobile {
-        align-items: center;
-        text-align: center;
-        // width: 100%;
-      }
-    }
-  }
-  &__center {
-    align-items: center;
-    justify-content: center;
-  }
-  &__right {
-    align-items: flex-end;
-    justify-content: flex-end;
-    @media @mobile {
-      align-items: center;
-      justify-content: center;
-    }
-    .overlay__content {
-      
-      align-items: flex-end;
-      text-align: right;
-      @media @mobile {
-        align-items: center;
-        text-align: center;
-      }
-    }
-  }
   &__image {
     display: none;
     max-width: 100%;
     margin-bottom: 10px;
     @media @mobile {
       display: flex;
+      height: 400px;
     }
   }
-  &__text {
-    font-size: 40px;
-    line-height: 49px;
-    font-weight: 700;
-    margin-bottom: 48px;
-    @media @tablet {
-      margin-bottom: 35px;
+}
+.overlay::before {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: var(--bg-color);
+  animation: b-ltr-before 1.5s cubic-bezier(0, 0, 0, 0) forwards;
+  background-color: white;
+}
+.overlay:nth-of-type(1)::before,
+.overlay:nth-of-type(1)::after {
+    animation-delay: 0.5s;
+}
+
+@keyframes b-ltr-after {
+    0% {
+        transform: translateY(-100%)
     }
-    @media @mobile {
-      font-size: 14px;
-      margin-bottom: 30px;
+
+    100% {
+        transform: translateY(101%)
     }
-  }
+}
+
+@keyframes b-ltr-before {
+    0% {
+        transform: translateY(0)
+    }
+
+    100% {
+        transform: translateY(-200%)
+    }
 }
 </style>
 
